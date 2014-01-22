1c8ef16725a13b9a0c76159de2a472df3675c965
Kelvin Leung <et.kelvin@gmail.com>
2014-01-22 18:15:09 +0800

    更改目录，上传Week III文档

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Computing-for-Data-Analysis-Assignments
-=======================================
+Computing for Data Analysis Assignments
+=
 
 Jan 6th to Feb 3rd
+
+---
+
+## Week II
+
+## Week III
+
+### Part II
+
+![Plot II]({{ site.url }}/Week III/Plots/part2.png)
---
+++ Week III/plotting.R
@@ -0,0 +1,15 @@
+# no idea why colClasses should equal to "character"
+mydata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+# store default par for later use
+opar <- par(no.readonly=T)
+# 3 rows of plots, 1 per row
+par(mfrow=c(3, 1))
+data.ha <- as.numeric(mydata[, 11])
+data.hf <- as.numeric(mydata[, 17])
+data.pn <- as.numeric(mydata[, 23])
+# set x axis' range
+plim <- range(data.ha, data.hf, data.pn, na.rm=T)
+# plot them all
+hist(data.ha, xlab="30-day Death Rate", xlim=plim, main="Heart Attact")
+hist(data.hf, xlab="30-day Death Rate", xlim=plim, main="Heart Failure")
+hist(data.pn, xlab="30-day Death Rate", xlim=plim, main="Pneumonia")
