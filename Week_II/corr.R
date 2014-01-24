5a70cb1126d2deb3a05143e8695f95b9bfae2c5b
Kelvin Leung <et.kelvin@gmail.com>
2014-01-24 15:21:41 +0800

    优化精简代码
    # Week II
    # Part III
    # Author: Kelvin
    # Version: optimized_1_0
    # Edit time: 2014/01/24

--- Week_II/corr.R
+++ Week_II/corr.R
@@ -1,3 +1,9 @@
+# Week II
+# Part III
+# Author: Kelvin
+# Version: optimized_1_0
+# Edit time: 2014/01/24
+
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
@@ -9,7 +15,7 @@ corr <- function(directory, threshold = 0) {
   
   ## Return a numeric vector of correlations
   
-  # 简述题目目的
+  # Brief goal:
   
   ## for each file in the directory 
   ## read the file 
@@ -21,23 +27,18 @@ corr <- function(directory, threshold = 0) {
   ## next file 
   ## return the numeric vector
   
-  # 初始化变量
-  
-  my_Cor <- vector(mode="numeric")
-  id <- 1:332
-  
-  for(i in id){
-    id_Mod <- sprintf("%03d" , as.numeric(i))
-    file_Name <- sprintf("%s/%s.csv" , directory , id_Mod)  
-    my_Data <- read.csv(file_Name)
-    n_Complete <- nrow(na.omit(my_Data))
-    if(n_Complete > threshold){
-      
-      # cor(use="pairwise.complete.obs")，直接处理缺失值
-      
-      my_Cor[i] <- cor(my_Data$sulfate,my_Data$nitrate,use="pairwise.complete.obs")
+  # Function for apply
+  SubCorr <- function(id){
+    filename <- sprintf("%s/%03d.csv", directory, as.numeric(id))
+    data <- read.csv(filename)  
+    if(nrow(na.omit(data)) > threshold){
+      # cor(use="pairwise.complete.obs") arg "use" can omit NAs
+      return(cor(data$sulfate, data$nitrate, use="pairwise.complete.obs"))
+    }else{
+      # NAs can be omitted later
+      return(NA)
     }
   }
-  
-  return(my_Cor)
+  # Return as a numeric vector
+  return(as.numeric(na.omit(sapply(1:332, SubCorr))))
 }
