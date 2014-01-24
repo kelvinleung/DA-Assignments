99198dbe3d9c8fa6aec718e5d5cf9ba7ec5bba63
Kelvin Leung <et.kelvin@gmail.com>
2014-01-24 11:26:00 +0800

    优化精简代码

--- Week_II/getmonitor.R
+++ Week_II/getmonitor.R
@@ -1,4 +1,6 @@
 getmonitor <- function(id, directory, summarize = FALSE) {
+  ## Author: Kelvin
+  
   ## 'id' is a vector of length 1 indicating the monitor ID
   ## number. The user can specify 'id' as either an integer, a
   ## character, or a numeric.
@@ -10,16 +12,15 @@ getmonitor <- function(id, directory, summarize = FALSE) {
   ## the data should be printed to the console; the default is
   ## FALSE
   
-  # 将id转为三位表示
-  
-  id_Mod <- sprintf("%03d" , as.numeric(id))       
-  file_Name <- sprintf("%s/%s.csv" , directory , id_Mod)  
+  # Turn id into 3-digit num, combine it with directory
+  # Abandoned code:
+  # id_Mod <- sprintf("%03d" , as.numeric(id))       
+  # file_Name <- sprintf("%s/%s.csv" , directory , id_Mod)  
+  filename <- sprintf("%s/%03d.csv", directory, as.numeric(id))
   my_Data <- read.csv(file_Name)
   if(summarize){
     print(summary(my_Data))
   }
-  
-  # 记得return
-  
+  # Return!!!
   return(my_Data)
 }
