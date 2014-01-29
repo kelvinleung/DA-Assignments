cdb0a30364bc6531d5cebe6cd5d300202d5f06fc
Kelvin Leung <et.kelvin@gmail.com>
2014-01-29 12:42:15 +0800

    删除旧代码

--- Week_III/plotting_part_3.R
+++ Week_III/plotting_part_3.R
@@ -3,22 +3,15 @@
 
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 outcome[, 11] <- as.numeric(outcome[, 11])
-# count the number of obs in each state
+# Count the number of obs in each state
 state.sub <- data.frame(n=as.vector(table(outcome$State)), state=names(table(outcome$State)))
-# suset, exclude those less than 20
-# tutorial: http://www.ats.ucla.edu/stat/r/modules/subsetting.htm
+# Suset, exclude those less than 20
+# Tutorial: http://www.ats.ucla.edu/stat/r/modules/subsetting.htm
 s <- state.sub[state.sub$n >= 20, 2]
 outcome2 <- outcome[outcome$State %in% s, ]
-# basic boxplot
+# Pick them out
 death <- outcome2[, 11]
 state <- outcome2$State
-
-# problem here:
-# abandoned code
-# sort the states by their median 30-day death rate and plot the boxplots in order of them
-# state.median <- names(sort(tapply(outcome2[, 11], outcome2$State, median, na.rm=T)))
-# state <- sort(factor(outcome2$State, levels=as.factor(state.median), order=T))
-
 # from forum: https://class.coursera.org/compdata-004/forum/thread?thread_id=926
 # reorder state, return as ordered fator
 state.median <- reorder(state, death, median, na.rm=T)
